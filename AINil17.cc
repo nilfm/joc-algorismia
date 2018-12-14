235e35dead72224c3504f337db021b3ae5587502
nilfm <nil.fons@gmail.com>
2018-12-14 17:44:42 +0100

    lol

--- AINil17.cc
+++ AINil17.cc
@@ -12,7 +12,7 @@
  * Write the name of your player and save this file
  * with the same name and .cc extension.
  */
-#define PLAYER_NAME duMMy
+#define PLAYER_NAME Nil17
 
 struct PLAYER_NAME : public Player {
     
